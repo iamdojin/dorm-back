015709b719449307e147cb27d88f772f7fef362d
kimdojin19 <kimdojin19@ewhain.net>
2022-06-08 09:58:56 +0900

    commit

---
+++ src/api/notice-post/routes/notice-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * notice-post router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::notice-post.notice-post');
---
+++ src/api/notice-post/services/notice-post.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * notice-post service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::notice-post.notice-post');
